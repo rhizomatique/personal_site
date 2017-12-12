eb66125c9f6e1bd3f78b3157ff00afcf46f75b21
schizoanalysis <31521820+schizoanalysis@users.noreply.github.com>
2017-12-11 21:35:01 -0600

    updates

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function openNav() {
-    document.getElementById("menu").style.width = "250px";
-    document.getElementById("content").style.marginLeft = "250px";
+  document.getElementById("content").style.marginLeft = "25%";
+  document.getElementById("menu").style.width = "25%";
+  document.getElementById("menu").style.display = "block";
+  document.getElementById("onav").style.display = 'none';
 }
-
 function closeNav() {
-    document.getElementById("menu").style.width = "0px";
-    document.getElementById("content").style.marginLeft = "0px";
+  document.getElementById("content").style.marginLeft = "0%";
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("onav").style.display = "inline-block";
 }
