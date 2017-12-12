c8e37157fbfd1b52bf0ce2fd9c64861556a93274
schizoanalysis <31521820+schizoanalysis@users.noreply.github.com>
2017-12-11 19:40:49 -0600

    updates

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("menu").style.width = "250px";
+    document.getElementById("content").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("menu").style.width = "0px";
+    document.getElementById("content").style.marginLeft = "0px";
+}
